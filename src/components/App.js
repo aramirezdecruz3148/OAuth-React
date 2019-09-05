6194b3a51cc1dbbba2703e55737870288c302e97
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-05 14:02:40 -0700

    got things set up and showing up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useAuth0 } from '../AuthOProvider';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const { isAuthenticated, user, loading, auth0Client } = useAuth0();
+  const login = () => auth0Client.loginWithRedirect();
+  const logout = () => auth0Client.logout();
+  return (
+    <>
+      { !isAuthenticated ? 
+        <button onClick={login}>Login</button>
+        : <button onClick={logout}>Logout</button>
+      }
+      <h1>YO</h1>
+    </>
+  );
 }
-  
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import AuthOProvider from './AuthOProvider';
 
 render(
-  <App />,
+  <AuthOProvider
+    domain='dev-de-cruz.auth0.com'
+    client_id='iQ20ICArUtJbBWqM6ZVgJAz9smpB4KP0'
+    redirect_uri='http://localhost:7890/'
+  >
+    <App />
+  </AuthOProvider>,
   document.getElementById('root')
 );
